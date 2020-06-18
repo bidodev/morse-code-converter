45a1ddab1efb7c927a9b5a071e73478fc85d4d00
Claudinei Bido <bido.c@live.com>
2020-06-18 12:17:48 +0200

    Updates

---
+++ precache-manifest.42f5dfb9ed0178f3ad2a8a2ab4c92693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2863cf997b44fb3832e8739cba212c61",
+    "url": "/morse-code-converter/index.html"
+  },
+  {
+    "revision": "9672b3489830fa2d6655",
+    "url": "/morse-code-converter/static/css/main.5e1e999c.chunk.css"
+  },
+  {
+    "revision": "edd44b254ac8de5bfde8",
+    "url": "/morse-code-converter/static/js/2.422db3d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/morse-code-converter/static/js/2.422db3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672b3489830fa2d6655",
+    "url": "/morse-code-converter/static/js/main.e0494141.chunk.js"
+  },
+  {
+    "revision": "9d7b83ed741317b10208",
+    "url": "/morse-code-converter/static/js/runtime-main.5b07644f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/morse-code-converter/precache-manifest.42f5dfb9ed0178f3ad2a8a2ab4c92693.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/morse-code-converter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
